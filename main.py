256cfb8d55fdded75d8f26f3e91ce9af579e2cda
nuo18 <kadamvedang18@gmail.com>
2023-08-03 15:09:04 +0700

    Buttons done. Progress bar sucks

--- main.py
+++ main.py
@@ -1,69 +1,72 @@
 from pytube import YouTube
 import customtkinter
+import threading
+import time
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("600x400")
-        #Centre the grid
-        self.grid_columnconfigure((0), weight=1)
-        self.grid_columnconfigure(0, weight=1)
+        self.geometry("800x550")
         
         #Title
-        self.label = customtkinter.CTkLabel(self, font=("Helvetica", 50), text="Youtube Downloader", fg_color="transparent")
-        self.label.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+        self.label = customtkinter.CTkLabel(self, font=("Helvetica", 50), text="Youtube downloader", fg_color="transparent")
+        self.label.pack(padx=20, pady=40)
 
-        
         #User enters link here
-        self.entry = customtkinter.CTkEntry(self, placeholder_text="Enter Youtube link here", width=300)
-        self.entry.grid(row=1, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+        self.entry = customtkinter.CTkEntry(self, placeholder_text="Enter Youtube link here", width=500)
+        self.entry.pack(padx=20, pady=20)
 
-        
-        #Download button
-        self.button = customtkinter.CTkButton(self, text="Download mp3", command=self.download)
-        self.button.grid(row=2, column=0, padx=20, pady=20, sticky="w")
-        self.button1 = customtkinter.CTkButton(self, text="Download mp4", command=self.download)
-        self.button1.grid(row=2, column=1, padx=20, pady=20, sticky="w")
+        #Download buttons
+        self.button_mp3 = customtkinter.CTkButton(self, text="Download mp3", command=self.download_audio)
+        self.button_mp3.pack(padx=20, pady=(0, 20))
 
-        
-        #Progress bar, changes according to value in set
-        self.progressbar = customtkinter.CTkProgressBar(self, orientation="horizontal", width=200, height=20)
-        self.progressbar.set(0.3)
-        self.progressbar.grid(row=3, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+        self.button_mp4 = customtkinter.CTkButton(self, text="Download mp4", command=self.download_video)
+        self.button_mp4.pack(padx=20, pady=(0, 20))
 
-    def download_mp3(self):
-        #Youtube object
+        #Bytes downloaded label
+        self.bytes_label = customtkinter.CTkLabel(self, text="", fg_color="transparent")
+        self.bytes_label.pack(padx=20, pady=20)
+
+    def download_audio(self):
+        self.download("audio")
+
+    def download_video(self):
+        self.download("video")
+
+    def on_progress(self, stream, chunk, bytes_remaining):
+        total_size = stream.filesize
+        bytes_downloaded = total_size - bytes_remaining
+        self.bytes_label.config(text=f"{bytes_downloaded}/{total_size} bytes downloaded")
+
+    def download(self, download_type):
         self.link = self.entry.get()
-        self.yt = YouTube(self.link)
+        self.yt = YouTube(self.link, on_progress_callback=self.on_progress)
 
-        #Showing title and size of the video
+        # Showing title and size of the video
         self.label2 = customtkinter.CTkLabel(self, text=self.yt.title, fg_color="transparent")
-        self.label2.grid(row=4, column=0, padx=20, pady=20)
+        self.label2.pack(padx=20, pady=20)
 
-        #Downloads video
-        self.video = self.yt.streams.get_highest_resolution()
-        self.video.download()
+        try:
+            if download_type == "audio":
+                # Downloads audio if available
+                self.audio = self.yt.streams.filter(only_audio=True).first()
+                if self.audio:
+                    self.audio.download()
+                else:
+                    raise Exception("No audio stream available for this video.")
+            elif download_type == "video":
+                # Downloads video
+                self.video = self.yt.streams.get_highest_resolution()
+                self.video.download()
 
-        #Done Icon
-        self.label3 = customtkinter.CTkLabel(self,text="Done!", fg_color="transparent")
-        self.label3.grid(row=5, column=0, padx=20, pady=20)
-    
-    def download_mp4(self):
-        #Youtube object
-        self.link = self.entry.get()
-        self.yt = YouTube(self.link)
+            # Done Icon
+            self.label3 = customtkinter.CTkLabel(self, text="Done!", fg_color="transparent")
+            self.label3.pack(padx=20, pady=20)
 
-        #Showing title and size of the video
-        self.label2 = customtkinter.CTkLabel(self, text=self.yt.title, fg_color="transparent")
-        self.label2.grid(row=4, column=0, padx=20, pady=20)
-
-        #Downloads video
-        self.video = self.yt.streams.get_highest_resolution()
-        self.video.download()
-
-        #Done Icon
-        self.label3 = customtkinter.CTkLabel(self,text="Done!", fg_color="transparent")
-        self.label3.grid(row=5, column=0, padx=20, pady=20)
+        except Exception as e:
+            # Error message
+            self.label3 = customtkinter.CTkLabel(self, text=f"Error: {str(e)}", fg_color="red")
+            self.label3.pack(padx=20, pady=20)
 
 app = App()
 app.mainloop()
